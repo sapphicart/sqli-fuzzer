5c8529e9d67a460a38b0ce8e1ab5b435049060d1
Shruti Priya <111532652+sapphicart@users.noreply.github.com>
2024-08-15 14:42:45 +0530

    updating version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name = 'sqlifuzzer',
-    version = '0.0.7',
+    version = '0.0.8',
     author = 'Shruti Priya',
     author_email = 'shrutipriya44@gmail.com',
     license = 'MIT Liecense',
